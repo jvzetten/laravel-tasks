e263e7df4e36cdbb37886e83b3c57d27853db06e
ZtJ <ZtJ@hoornbeeck.nl>
2023-10-03 12:24:09 +0200

    change tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -5,7 +5,7 @@
         <div class="col-sm-offset-2 col-sm-8">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    New Task
+                    New Task for ZtJ
                 </div>
 
                 <div class="panel-body">
